1ceee452cfa79adf5122f08cb2a681944ba2a88c
ash <alex@shintar.ru>
2025-04-27 17:34:11 +0300

    HW-05

---
+++ hw05-aop/src/main/java/ru/shintar/Main.java
@@ -0,0 +1,15 @@
+package ru.shintar;
+
+import ru.shintar.aop.api.TestLogging;
+import ru.shintar.aop.api.TestLoggingInterface;
+import ru.shintar.aop.proxy.Ioc;
+
+public class Main {
+    public static void main(String[] args) {
+        TestLoggingInterface target = new TestLogging();
+        TestLoggingInterface proxy = Ioc.createProxy(TestLoggingInterface.class, target);
+        proxy.calculation(6);
+        proxy.calculation(10, 20);
+        proxy.calculation(5, 7, "test");
+    }
+}
