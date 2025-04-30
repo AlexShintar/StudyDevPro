fcf07ba88a28f88c4a444330c9000fbcb8fa832a
ash <alex@shintar.ru>
2025-05-01 01:47:57 +0300

    HW-07

--- hw07-patterns/build.gradle.kts
+++ hw07-patterns/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation ("ch.qos.logback:logback-classic")
-
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
     testImplementation ("org.junit.jupiter:junit-jupiter-params")
--- hw07-patterns/src/main/java/ru/otus/HomeWork.java
+++ hw07-patterns/src/main/java/ru/otus/HomeWork.java
@@ -1,24 +1,47 @@
 package ru.otus;
 
+import java.time.LocalTime;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.ListenerPrinterConsole;
+import ru.otus.listener.homework.HistoryListener;
+import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.homework.ProcessorSwapField11toField12;
+import ru.otus.processor.homework.ProcessorThrowOnEvenSecond;
+
 public class HomeWork {
 
-    /*
-    Реализовать to do:
-      1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
-      2. Сделать процессор, который поменяет местами значения field11 и field12
-      3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
-            Секунда должна определяьться во время выполнения.
-            Тест - важная часть задания
-            Обязательно посмотрите пример к паттерну Мементо!
-      4. Сделать Listener для ведения истории (подумайте, как сделать, чтобы сообщения не портились)
-         Уже есть заготовка - класс HistoryListener, надо сделать его реализацию
-         Для него уже есть тест, убедитесь, что тест проходит
-    */
+    private static final Logger logger = LoggerFactory.getLogger(HomeWork.class);
 
     public static void main(String[] args) {
-        /*
-          по аналогии с Demo.class
-          из элеменов "to do" создать new ComplexProcessor и обработать сообщение
-        */
+
+        var processors = List.of(new ProcessorSwapField11toField12(), new ProcessorThrowOnEvenSecond(LocalTime::now));
+
+        var complexProcessor = new ComplexProcessor(processors, ex -> {});
+        var listenerPrinter = new ListenerPrinterConsole();
+        var historyListener = new HistoryListener();
+        complexProcessor.addListener(listenerPrinter);
+        complexProcessor.addListener(historyListener);
+
+        var message = Message.builder()
+                .id(2L)
+                .field11("field11")
+                .field12("field12")
+                .field13(new ObjectForMessage())
+                .build();
+
+        System.out.println(message);
+
+        var result = complexProcessor.handle(message);
+        logger.info("result:{}", result);
+
+        message = Message.builder().id(3L).field13(new ObjectForMessage()).build();
+
+        complexProcessor.handle(message);
+        complexProcessor.removeListener(listenerPrinter);
+        complexProcessor.removeListener(historyListener);
     }
 }
