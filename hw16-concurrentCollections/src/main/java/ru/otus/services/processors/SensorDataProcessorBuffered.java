5375716d9722ad92b36e26c4eff68c1665eef91f
ash <alex@shintar.ru>
2025-07-23 16:39:19 +0300

    HW-16

--- hw16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -1,36 +1,49 @@
 package ru.otus.services.processors;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
 import ru.otus.lib.SensorDataBufferedWriter;
 
-// Этот класс нужно реализовать
-@SuppressWarnings({"java:S1068", "java:S125"})
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
 
+    private final BlockingQueue<SensorData> dataBuffer;
+
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-        /*
-            if (dataBuffer.size() >= bufferSize) {
-                flush();
-            }
-        */
+        if (!dataBuffer.offer(data)) {
+            log.debug("Не удалось добавить данные в буфер: {}", data);
+        }
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
+        }
     }
 
     public void flush() {
         try {
-            // writer.writeBufferedData(bufferedData);
+            List<SensorData> bufferedData = new ArrayList<>(bufferSize);
+            dataBuffer.drainTo(bufferedData);
+            if (bufferedData.isEmpty()) {
+                log.debug("Нет данных для записи");
+                return;
+            }
+            writer.writeBufferedData(bufferedData);
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
