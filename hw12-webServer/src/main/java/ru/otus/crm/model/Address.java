ce99c03db462440b6c177ca696de751d56a7764c
ash <alex@shintar.ru>
2025-08-12 13:22:03 +0300

    HW-12: Copying templates

---
+++ hw12-webServer/src/main/java/ru/otus/crm/model/Address.java
@@ -0,0 +1,23 @@
+package ru.otus.crm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @SequenceGenerator(name = "address_gen", sequenceName = "address_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "address_street")
+    private String street;
+}
