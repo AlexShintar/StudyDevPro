fd8335fc9cbbab17420533e1736013e7488df5ff
ash <alex@shintar.ru>
2025-08-13 19:20:24 +0300

    HW-11

--- hw11-cache/src/main/java/ru/otus/cache/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cache/MyCache.java
@@ -1,30 +1,59 @@
 package ru.otus.cache;
 
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MyCache<K, V> implements HwCache<K, V> {
-    // Надо реализовать эти методы
+    private static final Logger log = LoggerFactory.getLogger(MyCache.class);
+
+    private final Map<K, V> cache = new WeakHashMap<>();
+
+    private final List<HwListener<K, V>> listeners = new CopyOnWriteArrayList<>();
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.put(key, value);
+        notifyListener(key, value, "put");
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = cache.remove(key);
+        if (value != null) {
+            notifyListener(key, value, "remove");
+        }
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        V value = cache.get(key);
+        if (value != null) {
+            notifyListener(key, value, "get");
+        }
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.remove(listener);
+    }
+
+    private void notifyListener(K key, V value, String action) {
+        listeners.forEach(listener -> {
+            try {
+                listener.notify(key, value, action);
+            } catch (Exception e) {
+                log.error("An error occurred while calling the listener.", e);
+            }
+        });
     }
 }
--- hw11-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw11-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cache.HwCache;
+import ru.otus.cache.HwListener;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionManager;
 import ru.otus.crm.model.Client;
@@ -14,31 +16,49 @@ public class DbServiceClientImpl implements DBServiceClient {
     private final DataTemplate<Client> clientDataTemplate;
     private final TransactionManager transactionManager;
 
-    public DbServiceClientImpl(TransactionManager transactionManager, DataTemplate<Client> clientDataTemplate) {
+    private final HwCache<String, Client> cache;
+
+    public DbServiceClientImpl(
+            TransactionManager transactionManager,
+            DataTemplate<Client> clientDataTemplate,
+            HwCache<String, Client> cache) {
         this.transactionManager = transactionManager;
         this.clientDataTemplate = clientDataTemplate;
+        this.cache = cache;
+        HwListener<String, Client> listener =
+                (key, value, action) -> log.info("notify: key={}, value={}, action={}", key, value, action);
+        this.cache.addListener(listener);
     }
 
     @Override
     public Client saveClient(Client client) {
         return transactionManager.doInTransaction(session -> {
             var clientCloned = client.clone();
-            if (client.getId() == null) {
-                var savedClient = clientDataTemplate.insert(session, clientCloned);
-                log.info("created client: {}", clientCloned);
-                return savedClient;
+            final Client savedClient;
+            if (clientCloned.getId() == null) {
+                savedClient = clientDataTemplate.insert(session, clientCloned);
+                log.info("created client: {}", savedClient);
+            } else {
+                savedClient = clientDataTemplate.update(session, clientCloned);
+                log.info("updated client: {}", savedClient);
             }
-            var savedClient = clientDataTemplate.update(session, clientCloned);
-            log.info("updated client: {}", savedClient);
+            cache.put("id" + savedClient.getId(), savedClient);
             return savedClient;
         });
     }
 
     @Override
     public Optional<Client> getClient(long id) {
+        Client cachedClient = cache.get("id" + id);
+        if (cachedClient != null) {
+            return Optional.of(cachedClient);
+        }
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientOptional = clientDataTemplate.findById(session, id);
-            log.info("client: {}", clientOptional);
+            clientOptional.ifPresent(found -> {
+                log.info("client: {}", found);
+                cache.put("id" + found.getId(), found);
+            });
             return clientOptional;
         });
     }
@@ -48,6 +68,7 @@ public class DbServiceClientImpl implements DBServiceClient {
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientList = clientDataTemplate.findAll(session);
             log.info("clientList:{}", clientList);
+            clientList.forEach(c -> cache.put("id" + c.getId(), c));
             return clientList;
         });
     }
